9901ae678af2826cb37ab28c70914ed80c9438f7
ykafia <kafia.youness@gmail.com>
2019-08-06 13:33:48 +0200

    Added GPT2 pretrained vocab

--- FSharp.Tokenizers/Library.fs
+++ FSharp.Tokenizers/Library.fs
@@ -1,5 +1,35 @@
 ﻿namespace FSharp.Tokenizers
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Tokenizers =
+    
+    type PretrainedFiles = {
+        VocabFiles:File;
+        MergeFiles:File;
+    }
+    type File = {
+        Name: string;
+        Url : string;
+    }
+    let GPT2PretrainedFiles = dict[
+        "vocab_files", dict[
+            "gpt2","https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json";
+            "gpt2-medium","https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-medium-vocab.json";
+        ];
+        "merge_files", dict[
+            "gpt2", "https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt";
+            "gpt2-medium", "https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-medium-merges.txt";
+        ];
+    ]
+
+    PretrainedPositionalEmbeddingSize = dict[
+        "gpt2", 1024;
+        "gpt2-medium",1024;
+    ]
+
+    VocabFileNames = dict[
+        "vocab_file": "vocab.json",
+        "merges_file": "merges.txt",
+    ]
+
+
+
